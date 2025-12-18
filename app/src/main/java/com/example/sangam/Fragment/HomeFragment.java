3597ace941b18302af4dea5beb8124fd444213ee
Sangam00007 <sangamtimalsina55@gmail.com>
2025-12-18 07:30:20 +0545

    Home Fragment Updated

--- app/src/main/java/com/example/sangam/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/sangam/Fragment/HomeFragment.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.sangam.R;
 
@@ -30,6 +31,14 @@ public class HomeFragment extends Fragment {
        View view = inflater.inflate(R.layout.fragment_home, container, false);
        Button btn = view.findViewById(R.id.btn_onclick);
        TextView title =  view.findViewById(R.id.tv_title);
+
+       btn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Toast.makeText(getContext(), "Home Fragment", Toast.LENGTH_SHORT).show();
+           }
+       });
+
        return view;
     }
 }
